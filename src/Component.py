8fd674f140754ec6aab23726ff9e4b97a76b9b0e
alien-gamma <scibot-dev@googlegroups.com>
2019-05-17 15:50:56 +0100

    Adds support for multi-sprite components in src/Component.py

--- src/Component.py
+++ src/Component.py
@@ -12,11 +12,11 @@ class Component(pygame.sprite.Sprite):
     """This class defines an individual Component."""
 
     def __init__(self,
-                 sprite,  # The image to display (can be None)
+                 sprite,  # The image (or list of images) to display (can be None)
                  start_logical_position,  # The starting point of the Component
                  step):  # The 'size' of the Component
         """Create a Component."""
-        # The sprite to display on screen for this Component
+        # The sprite (or list of sprites) to display on screen for this Component
         self.sprite = sprite
 
         # The position of the Component in terms of squares on the screen
@@ -25,14 +25,24 @@ class Component(pygame.sprite.Sprite):
         # The position of the Goal in terms pixels
         self.screen_location = start_logical_position.scale(step)
 
+        self.sprite_count = 0
+
         # calling superclass constructor
         pygame.sprite.Sprite.__init__(self)
+
+    def increment_sprite(self):
+        self.sprite_count = self.sprite_count+1
 
     def display(self, screen):
         """Draw the Component object on screen, if it has a sprite."""
         if self.sprite is not None:
-            screen.blit(self.sprite, (self.screen_location.x,
-                                      self.screen_location.y))
+            if type(self.sprite)==list:
+                if self.sprite_count==0:
+                    screen.blit(self.sprite[0], (self.screen_location.x, self.screen_location.y))
+                elif self.sprite_count==1:
+                    screen.blit(self.sprite[1], (self.screen_location.x, self.screen_location.y))
+            else:
+                screen.blit(self.sprite, (self.screen_location.x, self.screen_location.y))
 
     def is_equal_to(self, other_component):
         """Compare this Component for equality with other_component."""
